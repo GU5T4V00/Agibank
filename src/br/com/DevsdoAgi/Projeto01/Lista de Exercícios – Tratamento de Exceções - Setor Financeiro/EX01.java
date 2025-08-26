ca3d5fa0971f4239fec9c0a4d400f4f981aef729
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-08-26 11:00:39 -0300

    Update EX01.java

--- src/br/com/DevsdoAgi/Projeto01/Lista de Exercícios – Tratamento de Exceções - Setor Financeiro/EX01.java
+++ src/br/com/DevsdoAgi/Projeto01/Lista de Exercícios – Tratamento de Exceções - Setor Financeiro/EX01.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+/*
+1. Controle de Saldo de Contas
+Crie um programa que armazene os saldos de 10 contas bancárias em um vetor.
+ O usuário deve poder buscar o saldo de uma conta pelo índice do vetor.
+Se o usuário inserir um índice inválido, capture e trate ArrayIndexOutOfBoundsException.
+Exemplo de Entrada:
+Digite o índice da conta: 12
+Saída Esperada:
+Erro: Conta não encontrada!
+👉 Dica: Use try { ... } catch (ArrayIndexOutOfBoundsException e) { ... }.
+ */
+
+public class ControleDeContas {
+    public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+
+        // Vetor com saldo fictícios de 10 contas
+        double[] contas = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+        try{
+            System.out.println("Digite o índice da conta (0 a 9): ");
+            int indice = sc.nextInt();
+
+            // Pode gerar ArrayIndexOutOfBoundsException
+            double saldo = contas[indice];
+            System.out.printf("Saldo da conta %d: R$%.2f%n", indice, saldo);
+        }catch(ArrayIndexOutOfBoundsException erro){
+            System.out.println("ERRO: Conta não encontrada ou não existe.");
+        }
+        sc.close();
+    }
+}
