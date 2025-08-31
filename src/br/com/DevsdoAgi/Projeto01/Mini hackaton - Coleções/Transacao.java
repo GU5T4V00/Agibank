29d7f5b92636b1ad7285086464d740d788f0c575
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-08-31 09:12:03 -0300

    Update Transacao.java

--- src/br/com/DevsdoAgi/Projeto01/Mini hackaton - Coleções/Transacao.java
+++ src/br/com/DevsdoAgi/Projeto01/Mini hackaton - Coleções/Transacao.java
@@ -1,1 +1,21 @@
+import java.time.LocalDateTime;
 
+class Transacao {
+    private String tipo;
+    private double valor;
+    private LocalDateTime data;
+    private int contaOrigem, contaDestino;
+
+    public Transacao(String tipo, double valor, int contaOrigem, int contaDestino) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.contaOrigem = contaOrigem;
+        this.contaDestino = contaDestino;
+        this.data = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return data + " | " + tipo + " | Valor: " + valor + " | Origem: " + contaOrigem + " | Destino: " + contaDestino;
+    }
+}
