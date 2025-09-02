a1611a50862a0cc96ff143d767b5245759b8ea89
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-02 20:05:45 -0300

    Update EX03.java

--- src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX03.java
+++ src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX03.java
@@ -1,1 +1,22 @@
+import java.util.List;
+import java.util.stream.Collectors;
 
+/*
+3. Listar saldos negativos
+Com uma lista de saldos(tipo double), filtre
+e liste aqueles valores que são negativos.
+ */
+
+public class EX03 {
+    public static void main(String[] args) {
+        List<Double> lista = List.of(-23.54, 100.23, -2300.45, 500.00, -698.22, 459.87);
+
+        List<Double> numerosNegativos = lista.stream().filter(v -> v < 0).collect(Collectors.toList());
+
+        System.out.println("Os saldos negativos são respectivamente: " +
+                numerosNegativos.stream()
+                        .map(String::valueOf)
+                        .collect(Collectors.joining("|| ")));
+
+    }
+}
