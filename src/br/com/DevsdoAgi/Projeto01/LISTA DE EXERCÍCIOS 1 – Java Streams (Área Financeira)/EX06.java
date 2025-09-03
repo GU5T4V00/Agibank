c68e066264ed38a018300adb004e7e8c2e449a4b
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-03 10:52:02 -0300

    Update EX06.java

--- src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX06.java
+++ src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX06.java
@@ -1,1 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Comparator;
 
+/*
+Contar transações altas
+Conte quantos valores em uma lista são maiores que R$ 1.000,00.
+ */
+
+public class EX06 {
+    public static void main(String[] args){
+        // Lista de saldos
+        List<Double> saldos = List.of(-101.1, 5001.20, 1312.1, 2000.0, -3.7, 3070.40, 77.43);
+        List<Double> valoresAltos = saldos.stream().filter(v -> v > 1000.00).toList();
+        System.out.printf("Lista dos valores elevados: " + valoresAltos);
+    }
+}
