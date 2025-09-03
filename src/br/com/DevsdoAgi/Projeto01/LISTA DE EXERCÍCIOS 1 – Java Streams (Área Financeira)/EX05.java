639d055c800fd80d2e84adc194ddb91943af7abb
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-03 10:52:14 -0300

    Update EX05.java

--- src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX05.java
+++ src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX05.java
@@ -7,7 +7,7 @@ Transações em ordem decrescente
 Reordenar os valores fornecidos (tipo double) da lista em ordem decrescente.
  */
 
-public class EX01 {
+public class EX05 {
     public static void main(String[] args){
         // Lista de saldos
         List<Double> saldos = List.of(-101.1, 5001.20, 1312.1, 2000.0, -3.7, 3070.40, 77.43);
