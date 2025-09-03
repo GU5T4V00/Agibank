f06d71220d6b7053a979a75f944e95cc1dff347a
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-03 09:17:05 -0300

    Update EX04.java

--- src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX04.java
+++ src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX04.java
@@ -1,1 +1,15 @@
+import java.util.List;
 
+/*
+Média dos saldos
+Calcule a média de todos os saldos fornecidos. Se a lista estiver vazia, o resultado deve ser 0.
+ */
+
+public class EX04 {
+    public static void main(String[] args){
+        // Lista de saldos
+        List<Double> saldos = List.of(-101.1, 5001.20, 1312.1, 2000.0, -3.7, 3070.40, 6409.6);
+        double media = saldos.stream().mapToDouble(Double::doubleValue).average().orElse(0.00);
+        System.out.printf("A média de seus saldos é de: R$%.2f", media);
+    }
+}
