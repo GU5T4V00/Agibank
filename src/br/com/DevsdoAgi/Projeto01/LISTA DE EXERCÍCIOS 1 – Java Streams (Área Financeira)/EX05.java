a82e6fe81b778f0ae593386ce82e742d80d22cca
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-03 10:45:33 -0300

    Update EX05.java

--- src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX05.java
+++ src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX05.java
@@ -1,1 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Comparator;
 
+/*
+Transações em ordem decrescente
+Reordenar os valores fornecidos (tipo double) da lista em ordem decrescente.
+ */
+
+public class EX01 {
+    public static void main(String[] args){
+        // Lista de saldos
+        List<Double> saldos = List.of(-101.1, 5001.20, 1312.1, 2000.0, -3.7, 3070.40, 77.43);
+        String decrescente = saldos.stream().sorted(Comparator.reverseOrder()).map(String::valueOf).collect(Collectors.joining(" > "));
+        System.out.printf("Lista em ordem decrescente: " + decrescente);
+    }
+}
