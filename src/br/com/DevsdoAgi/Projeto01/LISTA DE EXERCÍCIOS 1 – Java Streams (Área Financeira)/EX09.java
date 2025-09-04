af41c0bd21d68360674e5dd74c5da441549bc0cf
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-04 08:58:08 -0300

    Update EX09.java

--- src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX09.java
+++ src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX09.java
@@ -1,1 +1,16 @@
+import java.util.List;
 
+/*
+Somar apenas valores positivos.
+Calcule a soma exclusiva dos valores positivos na lista.
+*/
+
+public class EX09 {
+    public static void main(String[] args){
+        // Lista de saldos
+        List<Double> saldos = List.of(-101.1, 15001.20, 1312.1, 2000.0, -3.7, 3070.40, 77.43);
+        double somaDosPositivos = saldos.stream().filter(v -> v >0).reduce(0.0, Double::sum);
+
+        System.out.printf("A soma dos valores positivos é: R$%.2f", somaDosPositivos);
+    }
+}
