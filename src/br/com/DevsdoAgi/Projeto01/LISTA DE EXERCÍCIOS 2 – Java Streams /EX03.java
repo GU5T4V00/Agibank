58aaa0e77f775edc2fd1de51e98fd4a2796ce334
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-04 10:01:20 -0300

    Update EX03.java

--- src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 2 – Java Streams /EX03.java
+++ src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 2 – Java Streams /EX03.java
@@ -1,1 +1,16 @@
+import java.util.List;
 
+/*
+Exercício 3 – Somar todos os elementos
+Operadores: +, reduce
+Tipo de retorno: Integer
+*/
+
+public class EX03 {
+    public static void main(String[] args){
+        // Lista de saldos
+        List<Integer> inteiros = List.of(1, 3, 5, 7, 9, 11, 13, 15, 17);
+        Integer somaDosInteiros = inteiros.stream().reduce(0, (a, b) -> a + b);
+        System.out.println("O resultado da soma é de: " + somaDosInteiros);
+    }
+}
