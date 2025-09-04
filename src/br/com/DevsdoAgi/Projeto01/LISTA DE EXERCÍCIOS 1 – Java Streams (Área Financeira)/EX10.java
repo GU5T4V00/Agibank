1e9136efa38c8323a5ddbd2adf88f4be4da013bc
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-04 09:06:29 -0300

    Update EX10.java

--- src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX10.java
+++ src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 1 – Java Streams (Área Financeira)/EX10.java
@@ -1,1 +1,15 @@
+import java.util.List;
 
+/*
+Produto multiplicativo das taxas de câmbio.
+Com uma lista de taxas (tipo double), calcule o produto acumulado de todas — ou seja, multiplique todos os valores.
+*/
+
+public class EX010 {
+    public static void main(String[] args){
+        // Lista de saldos
+        List<Double> taxas = List.of(1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10);
+        double multiplicacaoDasTaxas = taxas.stream().reduce(1.0, (a,b) -> a*b);
+        System.out.printf("A multiplicação de todas as taxas é de: R$%.2f", multiplicacaoDasTaxas);
+    }
+}
