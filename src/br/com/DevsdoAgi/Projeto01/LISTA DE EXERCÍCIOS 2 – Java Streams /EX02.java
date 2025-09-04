3ba398fcc34bbaae87f561837189d4ffffdb6138
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-04 09:34:23 -0300

    Update EX02.java

--- src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 2 – Java Streams /EX02.java
+++ src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 2 – Java Streams /EX02.java
@@ -1,1 +1,16 @@
+import java.util.List;
 
+/*
+Exercício 2 – Filtrar números maiores que 10
+Operadores: ->, >, >=, filter
+Tipo de retorno: Stream<Integer>
+*/
+
+public class EX02 {
+    public static void main(String[] args){
+        // Lista de saldos
+        List<Integer> inteiros = List.of(1, 3, 5, 7, 9, 11, 13, 15, 17);
+        List<Integer> inteirosMaioresQue10 = inteiros.stream().filter(v -> v > 10).toList();
+        System.out.println("Os numeros maiores que 10 são: " + inteirosMaioresQue10);
+    }
+}
