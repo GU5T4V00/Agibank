fef6f67f4f29ea8a6c64c0ff42cbd38fbf720a2f
GU5T4V00 <142633768+GU5T4V00@users.noreply.github.com>
2025-09-04 09:29:31 -0300

    Update EX01.java

--- src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 2 – Java Streams /EX01.java
+++ src/br/com/DevsdoAgi/Projeto01/LISTA DE EXERCÍCIOS 2 – Java Streams /EX01.java
@@ -1,1 +1,15 @@
+import java.util.List;
 
+/*
+Exercício 1 – Filtrar números pares
+Operadores: ->, ==, %, filter
+Tipo de retorno: Stream<Integer>
+*/
+
+public class EX01 {
+    public static void main(String[] args){
+        // Lista de saldos
+        List<String> estados = List.of("São Paulo", "Minas Gerais", "Acre", "Mato Grosso", "Paraná");
+        estados.stream().forEach(v -> System.out.println("Eu sou do estado de " + v));
+    }
+}
